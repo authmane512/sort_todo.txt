e32deb19f177b0effac5b5c0c99f56bb8d514ea4
Authmane Terki <authmane512@protonmail.ch>
2019-11-21 14:16:16 +0100

    initial commit

---
+++ sort_todo.py
@@ -0,0 +1,82 @@
+from pprint import pprint
+
+with open('todo.txt', 'r') as f:
+  tasks = f.readlines()
+
+tasks = [t for t in tasks if t.strip() != ""] # removes blank lines
+
+done_tasks = [t for t in tasks if t.startswith("x =")]
+tasks = [t for t in tasks if not t.startswith("x =")]
+with open('done.txt', 'a') as f:
+  f.write(''.join(done_tasks))
+
+def sort_tasks(tasks, char): # char is either @ or +
+  sorted_tasks = {}
+  for t in tasks:
+    # proj_or_context = ""
+    proj_or_context_list = []
+    words = t.split()
+    for w in words:
+      if w.startswith(char) and len(w) != 1:
+        proj_or_context.append(w)
+        # if proj_or_context.startswith("+_"):
+        #   break
+    #if proj_or_context == "" and char == "+":
+    #  t = t.rstrip() + " +main\n"
+    #  proj_or_context = "+main"
+
+    if proj_or_context not in sorted_tasks:
+      sorted_tasks[proj_or_context] = []
+    sorted_tasks[proj_or_context].append(t)
+
+  return sorted_tasks
+
+def sort_by_priority(tasks):
+  with_deadlines = []
+  with_letters = []
+  finished = []
+  others = []
+  for t in tasks:
+    if t[0:2] == "x ":
+      finished.append(t)
+    elif " due:" in t:
+      with_deadlines.append(t)
+    elif t[0] == '(' and t[2] == ')':
+      with_letters.append(t)
+    else:
+      others.append(t)
+
+  with_letters.sort(key=lambda x: x[1])
+
+  def get_date(task):
+    return [w for w in task.split() if w.startswith('due:')][0][4:]
+  with_deadlines.sort(key=get_date)
+
+  return with_deadlines + with_letters + others + finished
+
+sorted_by_proj = sort_tasks(tasks, "+")
+
+full_sorted = {}
+project_names = sorted(sorted_by_proj.keys(), key=str.lower)
+print(project_names)
+for proj in project_names:
+  sorted_by_context = sort_tasks(sorted_by_proj[proj], '@')
+  for context in sorted_by_context.keys():
+    sorted_by_context[context] = sort_by_priority(sorted_by_context[context])
+  full_sorted[proj] = sorted_by_context
+
+# pprint(full_sorted)
+
+new_file = ""
+for proj in project_names:
+  new_file += """
+x ================================================================================
+x = {}
+x ================================================================================\n""".format(proj.upper().replace('+', ''))
+
+  contexts = sorted(full_sorted[proj].keys())
+  for c in contexts:
+    new_file += "".join(full_sorted[proj][c]) + "\n\n"
+
+with open('todo.txt', 'w') as f:
+  f.write(new_file)
